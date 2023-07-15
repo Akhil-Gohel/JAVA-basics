8463a2816f87b6cfa93156ea028f5987d40da048
Akhil Gohel <akhilgohel.732003@gmail.com>
2023-07-15 23:28:06 +0530

    Hello World updated

--- src/firstprogram/HelloWorld.java
+++ src/firstprogram/HelloWorld.java
@@ -2,5 +2,6 @@ package firstprogram;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("updated");
     }
 }
