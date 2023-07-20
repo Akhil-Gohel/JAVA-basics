51050d4b36f39e54feeec0aa8163008c34844e98
Akhil Gohel <akhilgohel.732003@gmail.com>
2023-07-20 20:00:40 +0530

    Inheritance

--- src/objectorientedprogramming/inheritance/singlelevelinheritance/Dog.java
+++ src/objectorientedprogramming/inheritance/singlelevelinheritance/Dog.java
@@ -1,6 +1,6 @@
 package objectorientedprogramming.inheritance.singlelevelinheritance;
 
-public class Dog extends Animal{
+public class Dog extends Animal{ // Inheritance (Is-A Relationship) Dog is an Animal
     public void bark()
     {
         System.out.println("barking...");
